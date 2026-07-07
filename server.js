3ff287fe6a232ed4774c54452f4b8594f2a89d8f
ManauTunjae <manau.tunjae@hotmail.com>
2026-07-07 10:32:44 +0200

    feat: implement server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './server/config/db.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/health', (req, res) => {
+    res.json({status: 'ok'})    
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
