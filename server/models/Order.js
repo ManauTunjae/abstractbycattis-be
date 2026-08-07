ad573d5ce1eb2d46836e3da007a2c9185f4ac1cc
ManauTunjae <manau.tunjae@hotmail.com>
2026-08-07 15:33:50 +0200

    feat: implement orderSchema and export

--- server/models/Order.js
+++ server/models/Order.js
@@ -40,3 +40,33 @@ const orderCustomerSchema = new mongoose.Schema(
   },
   { _id: false },
 );
+
+const orderSchema = new mongoose.Schema(
+  {
+    customer: {
+      type: orderCustomerSchema,
+      required: true,
+      validate: {
+        validator: (arr) => arr.length > 0,
+        message: "Order must have at least one painting.",
+      },
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "paid", "shipped", "delivered", "cancelled"],
+      default: "pending",
+    },
+    shippingMethod: {
+      type: String,
+    },
+  },
+  { timestamp: true },
+);
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
