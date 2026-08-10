aa6d0054d992a8dbc5e0d75491489d19144b3b78
ManauTunjae <manau.tunjae@hotmail.com>
2026-08-10 10:26:30 +0200

    feat: implement user login with JWT token generate

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,27 +4,53 @@ import generateToken from "../utils/generateToken.js";
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const existingUser = await User.findOne({ email });
+    const existingUser = await user.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already existing." });
     }
-    const User = await User.create({
+
+    const user = await user.create({
       name,
       email,
       passwordHash: password,
     });
-    const token = generateToken(User._id, User.role);
+
+    const token = generateToken(user._id, user.role);
     res.status(201).json({
-      _id: User._id,
-      name: User.name,
-      email: User.email,
-      role: User.role,
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
       token,
     });
   } catch (error) {
     if (error.code === 11000) {
       return res.status(400).json({ message: "Epost already existing." });
     }
+    res.status(500).json({ message: "Server error.", error: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials." });
+    }
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials." });
+    }
+    const token = generateToken(user._id, user.role);
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token,
+    });
+  } catch (error) {
     res.status(500).json({ message: "Server error.", error: error.message });
   }
 };
